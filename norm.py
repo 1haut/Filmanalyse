dc661c4e9dcfcd66fb3ec4caf52d76fbfe1715c8
1haut <ozioma.marshall.anike@funkweb.org>
2025-06-23 17:02:53 +0200

    normalisation

--- norm.py
+++ norm.py
@@ -5,77 +5,91 @@ df = pd.read_csv("TMDB_filtered_movies.csv")
 
 persons_list = {}
 directors_list = {}
-directors_photo_list = {}
+photo_directors_list = {}
 producers_list = {}
 writers_list = {}
 production_companies_list = {}
 composers_list = {}
 
 
-# for i, row in df.itertuples():
-#     if row["id"] > 646460 and row["id"] < 646470:
-#         persons = row["cast"].strip().split(",")
-#         directors = row["directors"].strip().split(",")
-#         # producers = row["producers"].strip().split(",")
-#         # directors_of_photograpy = row["directors_of_photograpy"].strip().split(",")
-#         # writers = row["writers"].strip().split(",")
-#         # productions_companies = row["productions_companies"].strip().split(",")
-
-#         for person in persons:
-#             person = person.strip()
-#             if persons_list[person]:
-#                 persons_list[person].append(row["id"])
-#                 # persons_list[person] = 
-#                 # supabase.table("actors").insert({"name", person}).execute()
-#             else:
-#                 persons_list[person] = []
-
-#         for director in directors:
-#             director = director.strip()
-#             if directors_list[director]:
-#                 directors_list[director].append(row["id"])
-#             else:
-#                 directors_list[director] = []
-
-df.cast = df.cast.fillna('')
-df.director = df.director.fillna('')
-
-
 for row in df.itertuples():
-    # if row.id > 646460 and row.id < 646470:
-    if  row.id < 100:
-        print(row.id)
-        persons = row.cast.strip().split(",")
-        directors = row.director.strip().split(",")
-        # producers = row["producers"].strip().split(",")
-        # directors_of_photograpy = row["directors_of_photograpy"].strip().split(",")
-        # writers = row["writers"].strip().split(",")
-        # productions_companies = row["productions_companies"].strip().split(",")
+    persons = row.cast.strip().split(",")
+    directors = row.director.strip().split(",")
+    producers = row.producers.strip().split(",")
+    directors_of_photograpy = row.director_of_photography.strip().split(",")
+    writers = row.writers.strip().split(",")
+    production_companies = row.production_companies.strip().split(",")
+    composers = row.music_composer.strip().split(",")
 
-        # for person in persons:
-        #     person = person.strip()
-        #     if person in persons_list.keys():
-        #         persons_list[person].append(row.id)
-        #         # persons_list[person] = 
-        #         # supabase.table("actors").insert({"name", person}).execute()
-        #     elif person == '':
-        #         continue
-        #     else:
-        #         persons_list[person] = []
-        #         directors_list[director].append(row.id)
+    for person in persons:
+        person = person.strip()
+        if person == '':
+            continue
+        elif person in persons_list.keys():
+            persons_list[person].append(row.id)
+        else:
+            persons_list[person] = []
+            persons_list[person].append(row.id)
 
-        for director in directors:
-            director = director.strip()
-            if director == '':
-                continue
-            elif director in directors_list.keys():
-                directors_list[director].append(row.id)
-            else:
-                directors_list[director] = []
-                directors_list[director].append(row.id)
+    for director in directors:
+        director = director.strip()
+        if director == '':
+            continue
+        elif director in directors_list.keys():
+            directors_list[director].append(row.id)
+        else:
+            directors_list[director] = []
+            directors_list[director].append(row.id)
+
+    for producer in producers:
+        producer = producer.strip()
+        if producer == '':
+            continue
+        elif producer in producers_list.keys():
+            producers_list[producer].append(row.id)
+        else:
+            producers_list[producer] = []
+            producers_list[producer].append(row.id)
+
+    for photo_director in directors_of_photograpy:
+        photo_director = photo_director.strip()
+        if directors_of_photograpy == '':
+            continue
+        elif directors_of_photograpy in photo_directors_list.keys():
+            photo_directors_list[directors_of_photograpy].append(row.id)
+        else:
+            photo_directors_list[directors_of_photograpy] = []
+            photo_directors_list[directors_of_photograpy].append(row.id)
+
+    
+    for writer in writers:
+        writer = writer.strip()
+        if writer == '':
+            continue
+        elif writer in writers_list.keys():
+            writers_list[writer].append(row.id)
+        else:
+            writers_list[writer] = []
+            writers_list[writer].append(row.id)
+
+    for production_company in production_companies:
+        production_company = production_company.strip()
+        if production_company == '':
+            continue
+        elif production_company in production_companies_list.keys():
+            production_companies_list[production_company].append(row.id)
+        else:
+            production_companies_list[production_company] = []
+            production_companies_list[production_company].append(row.id)
 
 
-
-with open("result.txt", 'w') as file:
-    file.write(str(directors_list))
-
+    for composer in composers:
+        composer = composer.strip()
+        if composer == '':
+            continue
+        elif composer in composers_list.keys():
+            composers_list[composer].append(row.id)
+        else:
+            composers_list[composer] = []
+            composers_list[composer].append(row.id)
+        
